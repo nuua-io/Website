741add3faa007ec5e34a3cf56941ebacde6f948b
Erik Campobadal <soc@erik.cat>
2018-09-19 03:20:24 +0200

    Added title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name') }}</title>
+        <title>{{ config('app.name') }} Programming Language</title>
 
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
