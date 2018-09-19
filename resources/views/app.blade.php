2e8592cbab51571d77a2ad0efa85f8ab09950e69
Erik Campobadal <soc@erik.cat>
2018-09-19 02:24:56 +0200

    Fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,7 @@
             <div class="text-8xl uppercase text-white ">{{ config('app.name') }}</div>
             <div class="text-lg uppercase tracking-wide text-grey-darker">Programming Language</div>
             <div class="mt-12">
-<pre class="code leading-loose">
+<pre class="code leading-loose text-xs md:text-base">
 release_date = (language) => {
     return language == 'nuua' ? 2019 : 'Unknown'
 }
